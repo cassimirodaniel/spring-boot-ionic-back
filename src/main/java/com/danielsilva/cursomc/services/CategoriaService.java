23da32ea52c6c60067edd5339b0b14c385c96d19
Daniel Cassimiro <cassimiro.daniel@gmail.com>
2018-06-14 21:24:18 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/danielsilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/danielsilva/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.danielsilva.cursomc.domain.Categoria;
 import com.danielsilva.cursomc.repositories.CategoriaRepository;
+import com.danielsilva.cursomc.services.exception.ObjectNotFoundExecption;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundExecption(
+		"Objeto não encontrado! Id:" + id + ", Tipo: "
+		 + Categoria.class.getName()));
 	}
 
 }
---
+++ src/main/java/com/danielsilva/cursomc/services/exception/ObjectNotFoundExecption.java
@@ -0,0 +1,15 @@
+package com.danielsilva.cursomc.services.exception;
+
+public class ObjectNotFoundExecption extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundExecption(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundExecption(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
