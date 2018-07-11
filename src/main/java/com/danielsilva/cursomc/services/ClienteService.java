6cf9c8436f2d32f6aa1fb0e8bf2c3b949dadf17b
Daniel Cassimiro <cassimiro.daniel@gmail.com>
2018-07-10 21:42:44 -0300

    endpoint cliente id disponivel

---
+++ src/main/java/com/danielsilva/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.danielsilva.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danielsilva.cursomc.domain.Cliente;
+import com.danielsilva.cursomc.repositories.ClienteRepository;
+import com.danielsilva.cursomc.services.exception.ObjectNotFoundExecption;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundExecption(
+		"Objeto não encontrado! Id:" + id + ", Tipo: "
+		 + Cliente.class.getName()));
+	}
+
+}
