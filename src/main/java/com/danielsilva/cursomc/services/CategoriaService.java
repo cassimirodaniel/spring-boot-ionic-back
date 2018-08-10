fb9e84238b487bb7bf41ea26589c37a3b69c6208
Daniel Cassimiro <cassimiro.daniel@gmail.com>
2018-08-09 21:22:23 -0300

    Validacao sintatica com bean validation

--- src/main/java/com/danielsilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/danielsilva/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.danielsilva.cursomc.domain.Categoria;
+import com.danielsilva.cursomc.dto.CategoriaDTO;
 import com.danielsilva.cursomc.repositories.CategoriaRepository;
 import com.danielsilva.cursomc.services.exception.DataIntegrationExecption;
 import com.danielsilva.cursomc.services.exception.ObjectNotFoundExecption;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
